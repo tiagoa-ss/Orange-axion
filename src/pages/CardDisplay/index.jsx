b33338c0814f67d0ec75728517808da54294b3a1
tiagoa-ss <tsousa@alunos.utfpr.edu.br>
2022-06-27 20:10:38 -0300

    Card structure defined

--- src/pages/CardDisplay/index.jsx
+++ src/pages/CardDisplay/index.jsx
@@ -6,6 +6,19 @@ export function CardDisplay() {
 	return (
 		<>
 			<Header />
+			<div className='cardDisplay'>
+				<p className='cardTitle'>LIST OF FOODS</p>
+				<div className='cardGrid'>
+					{/* modelo de card
+					 <div className='cardGridItem'>
+						<img
+							src='https://t1.rg.ltmcdn.com/pt/posts/1/5/1/bife_na_chapa_2151_orig.jpg'
+							alt=''
+						/>
+						<p className='cardText'>Bife</p>
+					</div> */}
+				</div>
+			</div>
 		</>
 	)
 }
