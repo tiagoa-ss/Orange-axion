e11e951918683903562bd5bc4926610965fb99bd
tiagoa-ss <tsousa@alunos.utfpr.edu.br>
2022-06-28 09:40:22 -0300

    Card generated with api response

--- src/pages/CardDisplay/index.jsx
+++ src/pages/CardDisplay/index.jsx
@@ -1,14 +1,39 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
+import { urlBuilder } from '../../utils/urlBuilder'
 
 import './style.css'
 
 export function CardDisplay() {
+	const [cards, setCards] = useState([])
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:1337/foods')
+			.then((res) => {
+				console.log(res.data)
+				setCards(res.data)
+			})
+			.catch((err) => {
+				console.log(err)
+			})
+	}, [])
+
 	return (
 		<>
 			<Header />
 			<div className='cardDisplay'>
 				<p className='cardTitle'>LIST OF FOODS</p>
 				<div className='cardGrid'>
+					{cards.map((card) => {
+						return (
+							<div className='cardGridItem' key={card.id}>
+								<img src={urlBuilder(card.link.url)} alt='' />
+								<p className='cardText'>{card.name}</p>
+							</div>
+						)
+					})}
 					{/* modelo de card
 					 <div className='cardGridItem'>
 						<img
---
+++ src/utils/urlBuilder.js
@@ -0,0 +1,5 @@
+export function urlBuilder(src) {
+	const url = `http://localhost:1337${src}`
+
+	return url
+}
